941559fa886b9eac502cdf6f173a44800ba82501
Fouad Aoude <fouad@Fouads-MacBook-Pro.local>
2024-05-29 06:00:30 -0400

    fixed connect to db issues regarding folder

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
-try:
+try:        
     import sys
-    sys.path.append(1, '/Applications/MAMP/htdocs/Time-Boxing-AI/Database')       
-     
+    sys.path.insert(1, '/Applications/MAMP/htdocs/Time-Boxing-AI/Database/')           
 except:
     print("Issue Getting Database File")
-else:
-    from database import Database
+else:    
+    from db import Db
     print("Imported File Successfully")
+    
+conn = Db().connect()
